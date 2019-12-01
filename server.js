6292aa3d89ba118be6b102ea3fac6f04f9178b70
krittayot <nai-iiz@hotmail.co.th>
2019-12-01 23:34:03 +0700

    add html 1

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ app.get('/', function(req, res) {
 app.get('/home.html', function(req, res) {
     res.sendFile('home.html' , { root : __dirname});
 });
+app.get('/patient_manage.html', function(req, res) {
+    res.sendFile('patient_manage.html' , { root : __dirname});
+});
+app.get('/selected_person.html', function(req, res) {
+    res.sendFile('selected_person.html' , { root : __dirname});
+});
+app.get('/map_manage.html', function(req, res) {
+    res.sendFile('map_manage.html' , { root : __dirname});
+});
+
 
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
