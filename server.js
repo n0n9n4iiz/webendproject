5896b8cdeae62fcedbf17806f916217bfa2572d9
krittayot <nai-iiz@hotmail.co.th>
2019-12-01 23:23:35 +0700

    test up to heroku 1

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile('project62/index.html' , { root : __dirname});
 });
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
