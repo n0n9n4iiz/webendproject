006a59968194c5ec6c734f368725e8902d0a58e2
krittayot <nai-iiz@hotmail.co.th>
2019-12-01 23:21:17 +0700

    add send file

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var app = express();
-app.get('/', function (req, res) {
- res.send(JSON.stringify({ Hello: 'World'}));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
