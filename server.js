15d0a45e299341d74f47c7f15511dbd6000c6428
krittayot <nai-iiz@hotmail.co.th>
2019-12-01 23:28:37 +0700

    ได้ละ 3

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ var app = express();
 app.get('/', function(req, res) {
     res.sendFile('index.html' , { root : __dirname});
 });
+app.get('/home', function(req, res) {
+    res.sendFile('home.html' , { root : __dirname});
+});
+
 var port = process.env.PORT || 8080;
 app.listen(port, function () {
 console.log('App is runing on http://localhost:' + port);
